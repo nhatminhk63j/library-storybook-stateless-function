e6d676eb41a86cad02b6970a2542842a3ab08cc2
Nhat Minh <nhatngo11a1@gmail.com>
2020-05-29 07:28:25 +0700

    create component with function stateless

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import {Card, Button} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 
-class Book extends Component {
-    render() {
-        const {book} = this.props;
-        return (
-            <Card style={{ width: '15rem', margin: "1rem" }} className="text-center">
-                <Card.Img variant="top" src={book.cover} />
-                <Card.Body>
-                    <Card.Title> {book.title} </Card.Title>
-                    <Card.Text>
-                        {book.description}
-                    </Card.Text>
-                    <Button variant="primary">Buy now</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+function Book(props){
+    const {book} = props;
+    return (
+        <Card style={{ width: '15rem', margin: "1rem" }} className="text-center">
+            <Card.Img variant="top" src={book.cover} />
+            <Card.Body>
+                <Card.Title> {book.title} </Card.Title>
+                <Card.Text>
+                    {book.description}
+                </Card.Text>
+                <Button variant="primary">Buy now</Button>
+            </Card.Body>
+        </Card>
+    )
 }
 
 Book.propTypes = {
